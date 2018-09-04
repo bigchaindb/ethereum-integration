b3e901f5fc6ce45c603218a12cfaaad2dfdcbfc9
Shubhendu Shekhar <shubhendu@Shubhendus-MacBook-Pro.local>
2018-09-04 21:57:59 +0200

    working eth.js version

--- write-to-bdb/src/eth.js
+++ write-to-bdb/src/eth.js
@@ -1,63 +1,56 @@
 import Web3 from 'web3';
+import {default as contract} from 'truffle-contract';
 import tokenArtifact from '../build/contracts/Token.json';
 import globals from '../config/globals.config.json';
 
 const ETH_HOST = 'http://127.0.0.1:8545';
+const tokenContract = contract(tokenArtifact);
 const web3 = new Web3(new Web3.providers.HttpProvider(ETH_HOST));
 
-function deployContract(fromAddr, artifact, args = ['BDB Token', 'BDBT', 100000000]) {
-    return new web3.eth.Contract(artifact.abi).deploy({
-        data: artifact.bytecode,
-        arguments: args
-    }).send({
-        from: fromAddr,
-        gas: 1500000
-    });
+tokenContract.setProvider(new Web3.providers.HttpProvider(ETH_HOST));
+if (typeof tokenContract.currentProvider.sendAsync !== "function") {
+    tokenContract.currentProvider.sendAsync = function() {
+      return tokenContract.currentProvider.send.apply(
+        tokenContract.currentProvider, arguments
+      );
+    };
+  }
+
+export function deployToken(fromAddr, args = {name: 'BDB Token', symbol: 'BDBT', supply: 100000000}) {
+    tokenContract.defaults({from: fromAddr})
+    return tokenContract.new(args.name, args.symbol, args.supply, {from: fromAddr, gas: 5000000})
 }
 
-function getEthereumKeypair(passphrase = globals.userPasssphrse) {
-    return web3.eth.personal.newAccount(passphrase);
-}
-
-function unlockAccount(address, passphrase = globals.userPasssphrse, timeout = 1000) {
+export function unlockAccount(address, passphrase = globals.userPasssphrse, timeout = 1000) {
     return web3.eth.personal.unlockAccount(address, passphrase, timeout);
 }
 
-function fundAccount(to, from, ethers) {
+export function fundAccount(to, from, ethers) {
     web3.eth.sendTransaction({
         to,
         from,
         value: web3.utils.toWei(String(ethers), "ether")
     })
+};
+
+export function createAccount(passphrase = globals.userPasssphrse){
+    const account = web3.eth.accounts.create(web3.utils.randomHex(32))
+    return account.encrypt(passphrase);
 }
 
-let coinbase = '';
-web3.eth.getAccounts().then(arr => {
-    coinbase = arr[2]
-})
-web3.eth.getPastLogs({
-    address: '0x8d646a3a388aaece3732090d39ca4b9a7abf9dc7'
-}).then(console.log);
+export function decryptKeystore(keystoreJsonV3, password){
+    return web3.eth.accounts.decrypt(keystoreJsonV3, password);
+}
 
-getEthereumKeypair().then(address => {
-    fundAccount(address, coinbase, 10);
-    unlockAccount(address).then(flag => {
-        if (flag) {
-            deployContract(address, tokenArtifact)
-                .then(contract => {
-                    contract.events.Transfer({
-                        fromBlock: 0
-                    }, function (err, res) {
-                        console.log(err, res)
-                    });
-                    contract.methods.transfer('0x8d646a3a388aaece3732090d39ca4b9a7abf9dc7', 100).call({
-                            from: address
-                        })
-                        .then(console.log)
-                        .catch(err => console.log('Error: ', err));
-
-                })
-                .catch(err => console.log(err));
-        }
-    })
-});
+deployToken('0xfd3cf7b39e1301d5f0686e8760e68244a9cf18d1').then(
+    instance => {
+        instance.Transfer().watch((err, result)=>{
+            if(!err){
+                const {from , to, value } = result.args;
+                console.log(from, to, value);
+            }
+        })
+        instance.transfer('0xfd3cf7b39e1301d5f0686e8760e68244a9cf18d1', 10);
+        return instance;
+    }
+).catch(err => console.log(err));
