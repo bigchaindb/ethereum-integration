23494b71dee6b59d75abb09ba5283e3594f6fd04
Shubhendu Shekhar <shubhendu@shubhendus-mbp.lan>
2018-09-04 14:50:14 +0200

    added websocket provider

--- write-to-bdb/src/eth.js
+++ write-to-bdb/src/eth.js
@@ -1,9 +1,7 @@
 import Web3 from 'web3';
 import tokenArtifact from '../build/contracts/Token.json';
-import ganache from 'ganache-cli';
-const ETH_HOST = 'http://127.0.0.1:8545'
-const web3 = new Web3(ganache.provider());
-
+const ETH_HOST = 'http://127.0.0.1:8545';
+const web3 = new Web3(new Web3.providers.WebsocketProvider(ETH_HOST));
 
 function deployContract(fromAddr, artifact, args = ['BDB Token', 'BDBT', 100000000]){
     return new web3.eth.Contract(artifact.abi).deploy({data: artifact.bytecode, arguments: args}).send({
@@ -12,9 +10,10 @@ function deployContract(fromAddr, artifact, args = ['BDB Token', 'BDBT', 1000000
     });
 }
 
-deployContract('0x344abf1c45e8a68f387736aa381ba1843441bcb9', tokenArtifact)
+deployContract('0xd004c393b8daacb1cab95779331f803c8644bb9a', tokenArtifact)
 .then(contract => {
     // add publish token code here
+    console.log(contract._address);
     contract.events.Transfer((err, event) => {
         if(!err){
             //transfer token in bdb here
