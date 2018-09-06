3a9a889893061bb717c14c05ceefeaa35cbb7ded
Manan Patel <manan77patel@gmail.com>
2018-09-06 14:41:18 +0200

    added truck 2 simulation

--- poc1/truck/bdb.js
+++ poc1/truck/bdb.js
@@ -1,26 +1,32 @@
 const driver = require('bigchaindb-driver')
-const bip39 = require('bip39')
 const env = require('./env').default
 const API_PATH = env.bdb.url;
+const app_id = env.bdb.app_id;
+const app_key = env.bdb.app_key;
 
-export const sendLocation = (location) => {
+export const sendLocation = (truck, location) => {
+    let pubKey = env.bdb[truck + "PubKey"];
+    let privKey = env.bdb[truck + "PrivKey"];
     // Construct a transaction payload
     const tx = driver.Transaction.makeCreateTransaction(
         location,
         { lastModifiedOn: Date()},
         [ driver.Transaction.makeOutput(
-                driver.Transaction.makeEd25519Condition(env.bdb.pubKey))
+                driver.Transaction.makeEd25519Condition(pubKey))
         ],
-        env.bdb.pubKey
+        pubKey
     )
 
     // Sign the transaction with private keys
-    const txSigned = driver.Transaction.signTransaction(tx, env.bdb.privKey)
+    const txSigned = driver.Transaction.signTransaction(tx,privKey)
 
     // Send the transaction off to BigchainDB
-    const conn = new driver.Connection(API_PATH)
+    const conn = new driver.Connection(API_PATH, {
+        app_id: app_id,
+        app_key: app_key
+    });
 
     conn.postTransactionCommit(txSigned)
-        .then(retrievedTx => console.log('Truck successfully send its current location ('+ location.latitude +','
+        .then(retrievedTx => console.log(truck + ' successfully send its current location ('+ location.latitude +','
         + location.longitude +') to BigchainDB with tx id ', retrievedTx.id))
 }
--- poc1/truck/sendLocation.js
+++ poc1/truck/sendLocation.js
@@ -1,29 +1,44 @@
-const schedule = require('node-schedule')
-const bdb = require('./bdb')
-const env = require('./env').default
+const schedule = require('node-schedule');
+const bdb = require('./bdb');
 
 
-let longInc = 0.11370906666667, latInc = 0.034366866666667; //estimated arrival in 5 minutes (30 txs)
+let longInc = 0.11370906666667, latInc = 0.034366866666667; //estimated arrival in approx 3 minutes (30 txs)
 const latInit = 52.520008, longInit = 13.404954; //Berlin coordinates
 const latMax = 53.551086, longMin = 9.993682; //Hamburg coordinates
 let latitude = latInit, longitude = longInit;
+let truck = "truck1";
+let countsToExit = 10;
 
 // Execute every 10 seconds
-var j = schedule.scheduleJob('10 * * * * *', function () {
+var j = schedule.scheduleJob('*/5 * * * * *', function () {
 
+    //check if truck 1 reached Hamburg
     if(latitude >= latMax && longitude <= longMin){
-        console.log("Truck reached Hamburg...");
+        console.log(truck + " reached Hamburg...");
         latitude = latInit;
         longitude = longInit;
-        console.log("Truck will start again from Berlin to Hamburg...");
+        //Now, truck 2 will start the journey
+        truck = "truck2";
+        //truck 2 will go kaput midway between Berlin and Hamburg
+        countsToExit = 10; 
+        console.log(truck  + " will start from Berlin to Hamburg...");
     }
     else {
+        //truck rides towards Hamburg
         latitude += latInc;
-        longitude -= longInc;
+        longitude -= longInc; 
+    }
+    
+    //if truck 2 reaches midway to Hamburg, 
+    //it gets Kaput and stops sending location
+    if(countsToExit == 0 && truck == "truck2"){
+        console.log(truck  + " will got kaput in between Berlin to Hamburg...");
+        bdb.sendLocation(truck, location);
+        process.exit(0);
     }
 
+    countsToExit--;
     const location = {latitude, longitude, createdOn: Date()};
-    const transaction = bdb.sendLocation(location);
+    bdb.sendLocation(truck, location);
 
 })
-
