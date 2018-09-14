7d64b8c05b903e42a75da926073c37b9d6280428
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2018-09-14 18:08:27 +0200

    added truffle-contract and tests fixed.

--- poc2/src/eth.js
+++ poc2/src/eth.js
@@ -3,10 +3,24 @@ import jetpack from 'fs-jetpack'
 import tokenConfig from '../config/token.config.json';
 import network from '../config/network.config.json';
 import keys from '../config/keys.config.json';
+
+import {default as contract } from 'truffle-contract';
+import tokenArtifact from '../build/contracts/Token.json'
+
 import bdb from './bdb'
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider(network.ethereum.host));
-const contract = new web3.eth.Contract(tokenConfig.abi, tokenConfig.address);
+const tokenContract = contract(tokenArtifact);
+tokenContract.setProvider(new Web3.providers.HttpProvider(network.ethereum.host));
+tokenContract.defaults({from: network.ethereum.fromAddress})
+if (typeof tokenContract.currentProvider.sendAsync !== "function") {
+    tokenContract.currentProvider.sendAsync = function() {
+      return tokenContract.currentProvider.send.apply(
+        tokenContract.currentProvider, arguments
+      );
+    };
+  }
+
+
 //Define bigchaindb asset
 const asset = {
     name: tokenConfig.name,
@@ -15,7 +29,7 @@ const asset = {
     timestamp: Date.now()
 }
 
-let bdbId = tokenConfig.bigchaindbId;
+let bdbId = tokenConfig.bdbId;
 
 async function mintAndListen() {
     // Mint token in BigchainDB if id not present
@@ -25,14 +39,16 @@ async function mintAndListen() {
         updateConfig('bdbId', bdbId);
     }
     //Listen to the event (in this case Transfer) and replicate the transaction on bigchaindb 
-    contract.events.Transfer({}, async function (err, event) {
+    tokenContract.deployed().then(contract => {
+        contract.Transfer(async (err, event) => {
         if (!err) {
             const newBdbUser = bdb.createKeypair(keys.userPasssphrse)
-            await bdb.transferTokens(keys.adminPassphrase, bdbId, event.returnValues.value, newBdbUser.publicKey)
+            await bdb.transferTokens(keys.adminPassphrase, bdbId, event.args.value, newBdbUser.publicKey)
         } else {
             console.log(err);
         }
-    });
+    })
+    })
 };
 
 async function updateConfig(id, value) {
--- poc2/test/TestToken.js
+++ poc2/test/TestToken.js
@@ -1,18 +1,22 @@
 import Web3 from 'web3';
-import tokenConfig from '../config/token.config.json';
 import network from '../config/network.config.json';
+import {default as contract } from 'truffle-contract';
+import tokenArtifact from '../build/contracts/Token.json'
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider(network.ethereum.host));
-const contract = new web3.eth.Contract(tokenConfig.abi, tokenConfig.address);
+//const contract = new web3.eth.Contract(tokenConfig.abi, tokenConfig.address);
 
-//console.log('Result: ',result);
+const tokenContract = contract(tokenArtifact);
+tokenContract.setProvider(new Web3.providers.HttpProvider(network.ethereum.host));
+tokenContract.defaults({from: '0x3cf3a1d91c8628d33aaf5184ced04c55c437a0df'})
+if (typeof tokenContract.currentProvider.sendAsync !== "function") {
+    tokenContract.currentProvider.sendAsync = function() {
+      return tokenContract.currentProvider.send.apply(
+        tokenContract.currentProvider, arguments
+      );
+    };
+  }
 
-contract.events.Transfer({
-    fromBlock: 0,
-    toBlock: 'latest'
-}, async function (err, event) {
-    console.log(err, event);
-});
-
-const result = contract.methods.transfer('0x7b39b8bedd76753e8b7657bce9d6fc42fdc30d78', 10);
-console.log('tx executed');
+tokenContract.deployed().then(contract => {
+contract.transfer('0x7b39b8bedd76753e8b7657bce9d6fc42fdc30d78', 10)
+})
