5229417ff3bd61926fb16a4375dc192dedfd97d3
Gautam Dhameja <gautamdhameja@users.noreply.github.com>
2018-09-14 15:08:55 +0200

    updated poc1

--- poc1/oracle/contracts/BdbAdapter.sol
+++ poc1/oracle/contracts/BdbAdapter.sol
@@ -4,17 +4,10 @@ import "oraclize-api/usingOraclize.sol";
 
 contract BdbAdapter is usingOraclize {
     address public owner;
-    enum stageType {outputs, asset}
 
     struct pendingOperation {
-        stageType stage;
         address receiver;
         uint256 amount;
-        bool latitude; // If true latitude, if false longitud
-        uint256 value;
-        bytes32 otherQuery;
-        bool latOkay;
-        bool longOkay;
     }
 
     // modifier
@@ -28,118 +21,66 @@ contract BdbAdapter is usingOraclize {
 
     // bigchaindb node url
     string public apiUrl = "https://test.bigchaindb.com";
+    string public locationAddress = "Av. de Castilla y León, 109006 Burgos";
 
     // bigchaindb API query components
     string constant public apiStart = "json(";
     string constant public apiOutputsEndpoint = "/api/v1/outputs?spent=false&public_key=";
     string constant public apiOutputsClose = ").0.output_index";
     string constant public apiAssetEndpoint = "/api/v1/transactions/";
-    string constant public apiAssetClose = ").asset.data";
-    string constant public assetLongitude = ".longitude";
-    string constant public assetLatitude = ".latitude";
+    string constant public apiAssetClose = ").asset.data.address";
 
     // events
-    event newAssetQuery(string assetQuery);
-    event newAssetResult(uint256 assetResult);
-    event newOutputQuery(string outputQuery);
-    event newOutputResult(uint256 outputResult);
-    event nodeUrlChanged(string apiUrl);
-    event queryPrice(uint256 num);
+    event newOutputResult(string outputResult);
+    event queryParametersChanged(string apiUrl, string locationAddress);
 
-    constructor(string apiUrlValue) public {
+    constructor(string _apiUrlValue, string _locationAddress) public {
         // set _owner
         owner = msg.sender;
 
-        // set BigchainDB node url
-        apiUrl = apiUrlValue;
+        // set query params
+        apiUrl = _apiUrlValue;
+        locationAddress = _locationAddress;
     }
 
     // changes the url for BigchainDB node
     // in case there is a need for querying another node
     // owner only
-    function changeApiUrl(string apiUrlValue) public onlyOwner {
+    function changeQueryParameters(string _apiUrlValue, string _locationAddress) public onlyOwner {
         // set new BigchainDB node url
-        apiUrl = apiUrlValue;
-        emit nodeUrlChanged(apiUrlValue);
+        apiUrl = _apiUrlValue;
+        locationAddress = _locationAddress;
+        emit queryParametersChanged(_apiUrlValue, _locationAddress);
     }
 
     // Send payment
-    function sendPayment(string _bigchaindbOwner, address _receiver, uint256 _amount, uint256 latitude, uint256 longitude) public payable {
+    function sendPayment(string _bigchaindbTxId, address _receiver, uint256 _amount) public payable {
         // check msg.amount (needs to include payment + oracle gas)
+        // needs to be optimized for gas
         require(_amount < msg.value, "Not enough amount.");
-        outputs(_bigchaindbOwner, _receiver, _amount, latitude, longitude);
+        getAsset(_bigchaindbTxId, _receiver, _amount);
     }
 
     // Query from outputs - internal
-    function outputs(string _bigchaindbOwner, address _receiver, uint256 _amount, uint256 latitude, uint256 longitude) internal {
-        string memory query = strConcat(apiStart, apiUrl, apiAssetEndpoint, _bigchaindbOwner, apiAssetClose);
-        string memory queryLatitude = strConcat(query, assetLatitude);
-        string memory queryLongitude = strConcat(query, assetLongitude);
-        emit newAssetQuery(queryLatitude);
-        emit queryPrice(oraclize_getPrice("URL"));
-        bytes32 idLat = oraclize_query("URL", queryLatitude);
-        bytes32 idLong = oraclize_query("URL", queryLongitude);
-        pendingOperations[idLat] = pendingOperation(stageType.asset, _receiver, _amount, true, latitude, idLong, false, false);
-        pendingOperations[idLong] = pendingOperation(stageType.asset, _receiver, _amount, false, longitude, idLat, false, false);
-    }
-
-    // Query from asset - internal
-    function asset(string _bdbAssetId, address _receiver, uint256 _amount, uint256 latitude, uint256 longitude) internal {
-        string memory query = strConcat(apiStart, apiUrl, apiAssetEndpoint, _bdbAssetId, apiAssetClose);
-        emit newOutputQuery(query);
+    function getAsset(string _bigchaindbTxId, address _receiver, uint256 _amount) internal {
+        string memory query = strConcat(apiStart, apiUrl, apiAssetEndpoint, _bigchaindbTxId, apiAssetClose);
         bytes32 id = oraclize_query("URL", query);
-        //pendingOperations[id] = pendingOperation(stageType.asset, _receiver, _amount, true, longitude, false, false);
+        pendingOperations[id] = pendingOperation(_receiver, _amount);
     }
 
     // Result from oraclize
     function __callback(bytes32 id, string stringResult) public {
-        uint256 result = stringToUint(stringResult);
-
         require(msg.sender == oraclize_cbAddress(), "Access Denied.");
         require(pendingOperations[id].amount > 0, "Not enough amount.");
-        emit newOutputResult(result);
+        emit newOutputResult(stringResult);
         
-        stageType stage = pendingOperations[id].stage;
         address receiver = pendingOperations[id].receiver;
         uint256 amount = pendingOperations[id].amount;
 
-        if(stage == stageType.outputs){
-            emit newOutputResult(result);
-            //asset("", receiver, amount, pendingOperations[id].latitude, pendingOperations[id].longitude);
-
-        } else if(stage == stageType.asset){
-
-            //If latitude
-            if(pendingOperations[id].latitude){
-                if(pendingOperations[id].value + 5 > result && pendingOperations[id].value -5 < result){
-                    pendingOperations[id].latOkay = true;
-                    pendingOperations[pendingOperations[id].otherQuery].latOkay = true;
-                }
-
-            }else{ // If longitude
-                if(pendingOperations[id].value + 5 > result && pendingOperations[id].value -5 < result){
-                    pendingOperations[id].longOkay = true;
-                    pendingOperations[pendingOperations[id].otherQuery].longOkay = true;
-                }
-            }
-            // If lat and long are okay, release payment
-            if(pendingOperations[id].latOkay && pendingOperations[id].longOkay){
-                receiver.transfer(amount);
-            }
+        if(keccak256(locationAddress) == keccak256(stringResult)){
+            receiver.transfer(amount);
         }
 
         delete pendingOperations[id];
-    }
-
-    function stringToUint(string s) constant returns (uint result) {
-        bytes memory b = bytes(s);
-        uint i;
-        result = 0;
-        for (i = 0; i < b.length; i++) {
-            uint c = uint(b[i]);
-            if (c >= 48 && c <= 57) {
-                result = result * 10 + (c - 48);
-            }
-        }
     }
 }
--- poc1/truck/sendLocation.js
+++ poc1/truck/sendLocation.js
@@ -1,44 +1,13 @@
-const schedule = require('node-schedule');
 const bdb = require('./bdb');
 
-
-let longInc = 0.11370906666667, latInc = 0.034366866666667; //estimated arrival in approx 3 minutes (30 txs)
-const latInit = 52.520008, longInit = 13.404954; //Berlin coordinates
-const latMax = 53.551086, longMin = 9.993682; //Hamburg coordinates
-let latitude = latInit, longitude = longInit;
+const berlinAddress = "Chausseestraße 19, 10115 Berlin";
+const burgosAddress = "Av. de Castilla y León, 109006 Burgos";
 let truck = "truck1";
-let countsToExit = 10;
 
-// truck sends location every 5 seconds
-var j = schedule.scheduleJob('*/5 * * * * *', function () {
+export function sendBerlinAddress() {
+    bdb.sendLocation(truck, { address: berlinAddress});
+}
 
-    //check if truck 1 reached Hamburg
-    if(latitude >= latMax && longitude <= longMin){
-        console.log(truck + " reached Hamburg...");
-        latitude = latInit;
-        longitude = longInit;
-        //Now, truck 2 will start the journey
-        truck = "truck2";
-        //truck 2 will go kaput midway between Berlin and Hamburg
-        countsToExit = 10; 
-        console.log(truck  + " will start from Berlin to Hamburg...");
-    }
-    else {
-        //truck rides towards Hamburg
-        latitude += latInc;
-        longitude -= longInc; 
-    }
-    
-    //if truck 2 reaches midway to Hamburg, 
-    //it gets Kaput and stops sending location
-    if(countsToExit == 0 && truck == "truck2"){
-        console.log(truck  + " got kaput in between Berlin to Hamburg...");
-        bdb.sendLocation(truck, location);
-        process.exit(0);
-    }
-
-    countsToExit--;
-    const location = {latitude, longitude, createdOn: Date()};
-    bdb.sendLocation(truck, location);
-
-})
+export function sendBurgosAddress() {
+    bdb.sendLocation(truck, { address: burgosAddress});
+}
