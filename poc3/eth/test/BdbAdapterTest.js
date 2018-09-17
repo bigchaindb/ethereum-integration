cdb80effc2a75f3d52687226146fd4d6b65a0429
Manan Patel <manan77patel@gmail.com>
2018-09-17 22:56:23 +0200

    added tests

---
+++ poc3/eth/test/BdbAdapterTest.js
@@ -0,0 +1,37 @@
+var Web3 = require('web3');
+const { waitForEvent } = require('./utils');
+var BdbAdapter = artifacts.require('BdbAdapter');
+var testparams = require('./TestParam.json');
+
+var web3 = new Web3(new Web3.providers.HttpProvider(testparams.ethereum.host));
+
+contract('BigchainDB Adapter Contract Test', () => {
+    describe('End-To-End POC3 Oraclize Test', async () => {
+        it('Should log new output result event and balance should be updated', async () => {
+            const {
+                contract
+            } = await BdbAdapter.deployed();
+            const expected = new web3.utils.BN(testparams.queryEngine.expected_value);
+            let balance = 0;
+            web3.eth.getBalance(testparams.ethereum.to_address).then(result => {balance = result});
+            contract.sendPayment(testparams.bigchaindb.owner, testparams.ethereum.to_address, testparams.ethereum.amount,"","", {
+                from: testparams.ethereum.from_address,
+                gas: testparams.ethereum.gas,
+                value: testparams.ethereum.value
+            }), {
+                args: {
+                    outputResult
+                }
+            } = await waitForEvent(contract.NewOutputResult({}, {
+                fromBlock: 0,
+                toBlock: 'latest'
+            }));
+            
+            web3.eth.getBalance(testparams.ethereum.to_address).then(newBalance => {
+                assert.equal(outputResult, expected);
+                assert.equal(newBalance, balance + testparams.ethereum.amount);
+            });
+            
+        });
+    })
+});
